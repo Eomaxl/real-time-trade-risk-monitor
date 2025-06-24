ee4343795590df2daa0a8c52954bf17a38304802
Eomaxl <souravsnigdh@gmail.com>
2025-06-25 00:35:10 +0530

    [trading-app-backend] : Completed entity module for audit and common

--- trader-app-backend/src/main/java/com/eomaxl/traderappbackend/entity/audit/AuditLog.java
+++ trader-app-backend/src/main/java/com/eomaxl/traderappbackend/entity/audit/AuditLog.java
@@ -1,23 +1,37 @@
 package com.eomaxl.traderappbackend.entity.audit;
 
 import com.eomaxl.traderappbackend.entity.auth.User;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.sql.Timestamp;
+import java.time.Instant;
 
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table( name = "audit_logs")
 public class AuditLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
+
     private String action;
     private String entityType;
     private String entityId;
+
+    @Lob
     private String details;
-    private Timestamp timestamp;
+
+    private Instant timestamp;
 }
--- trader-app-backend/src/main/java/com/eomaxl/traderappbackend/entity/common/AppConfig.java
+++ trader-app-backend/src/main/java/com/eomaxl/traderappbackend/entity/common/AppConfig.java
@@ -1,4 +1,24 @@
 package com.eomaxl.traderappbackend.entity.common;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "app_config")
 public class AppConfig {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String configKey;
+
+    private String configValue;
 }
